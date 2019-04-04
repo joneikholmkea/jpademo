622717c4f296f3ab290648c31049a0edd887b50e
joneikholm <joneikholm@gmail.com>
2019-04-04 11:01:44 +0200

    2nd

--- src/main/java/com/example/jpademo/controller/HomeController.java
+++ src/main/java/com/example/jpademo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.jpademo.controller;
 
+import com.example.jpademo.model.Difficulty;
 import com.example.jpademo.model.Recipe;
 import com.example.jpademo.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,14 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        Optional<Recipe> recipe = recipeRepository.findByDescription("Perfect Guacamole");
-        if (recipe.isPresent()){
-            System.out.println("fandt: " + recipe.get().getDescription());
-        }else {
-            System.out.println("fandt ingen opskrift");
-        }
+
+//        System.out.println(Difficulty.AreYouNuts);
+//        Optional<Recipe> recipe = recipeRepository.findByDescription("Perfect Guacamole");
+//        if (recipe.isPresent()){
+//            System.out.println("fandt: " + recipe.get().getDescription());
+//        }else {
+//            System.out.println("fandt ingen opskrift");
+//        }
         return "index";
     }
     @PostMapping("/opret")
---
+++ src/main/java/com/example/jpademo/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.example.jpademo.model;
+
+public enum Difficulty {
+    Easy, Medium, Difficult, AreYouNuts
+}
